e8a3baed1da4608cf019f8d54bc59721bf0412b3
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-05-03 21:18:14 -0300

    create selectScreen

---
+++ lib/screens/selectGameScreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:image_and_action/screens/pageGameScreen.dart';
+
+class SelectGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue[200],
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            buttonTypeGame(context, 'TUDO'),
+            buttonTypeGame(context, 'ANIMAIS'),
+            buttonTypeGame(context, 'OBJETOS'),
+            buttonTypeGame(context, 'VERBOS'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget buttonTypeGame(context, String type) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: MaterialButton(
+      child: Container(
+        height: 70,
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Text(type),
+      ),
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => PageGame(type),
+          ),
+        );
+      },
+    ),
+  );
+}
