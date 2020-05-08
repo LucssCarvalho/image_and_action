a55ca56cc15678bb832c2260b9d308d9a8b66c24
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-05-07 23:17:06 -0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'image and action',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.green,
       ),
       home: SelectGame(),
     );
